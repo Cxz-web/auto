dade01c745f212d5aa5778aabe20dd9f17fc09b8
XinZhu <645870625@qq.com>
2020-06-28 18:09:59 +0800

    fix: 3

--- auto.js
+++ auto.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/', secret: '8562871' })
+var handler = createHandler({ path: '/update', secret: '8562871' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
 function run_cmd(cmd, args, callback) {
@@ -13,7 +13,6 @@ function run_cmd(cmd, args, callback) {
 }
 
 http.createServer(function (req, res) {
-  console.log(req)
   handler(req, res, function (err) {
     res.statusCode = 404
     res.end('no such location')
